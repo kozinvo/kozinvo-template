26fa46ec31b088e288b9c8419ddd8af01677ddf5
Kozin Valentin <kozinvo333@gmail.com>
2023-04-01 01:08:04 +0500

    burger finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,24 @@
-const sum = (a, b) => a + b
-console.log(sum(2, 4));
+document.addEventListener('click', documentActions);
+function documentActions(e) {
+  const targetEl = e.target;
+  if (targetEl.closest('.icon-menu')) {
+    document.documentElement.classList.toggle('menu-open');
+  }
+
+  if (targetEl.closest('[data-goto]')) {
+    e.preventDefault();
+
+    document.documentElement.classList.contains('menu-open') ? document.documentElement.classList.remove('menu-open') :
+      null;
+
+    const goTo = targetEl.closest('[data-goto]').dataset.goto;
+    const goToEl = document.querySelector(goTo);
+    const headerHeight = document.querySelector('.header').offsetHeight;
+    if (goToEl) {
+      window.scrollTo({
+        top: goToEl.offsetTop - (headerHeight + 15),
+        behavior: "smooth"
+      })
+    }
+  }
+}
