579848cf6727aa82e6245669fc7bccc48959bdec
Kozin Valentin <kozinvo333@gmail.com>
2023-03-31 20:12:24 +0500

    add sections and header html

--- gulpfile.js/task/js.js
+++ gulpfile.js/task/js.js
@@ -19,16 +19,16 @@ const uglify = require('gulp-uglify')
 
 const js = () => {
   return src(path.js.src, {
-      sourcemaps: app.isDev
-    })
+    sourcemaps: app.isDev
+  })
     .pipe(plumber({
       errorHandler: notify.onError(error => ({
         title: "Javascript",
         message: error.message
       }))
     }))
-    .pipe(babel())
-    .pipe(uglify())
+    // .pipe(babel())
+    // .pipe(uglify())
     .pipe(dest(path.js.dest, {
       sourcemaps: app.isDev
     }))
