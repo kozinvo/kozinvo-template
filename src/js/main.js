00aec633f1b37001661292b3cc9dc01c442e2620
Kozin Valentin <kozinvo333@gmail.com>
2023-04-01 18:06:48 +0500

    add home

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,14 @@
 document.addEventListener('click', documentActions);
 function documentActions(e) {
   const targetEl = e.target;
+  // burger menu
   if (targetEl.closest('.icon-menu')) {
     document.documentElement.classList.toggle('menu-open');
   }
-
+  //ahchor links
   if (targetEl.closest('[data-goto]')) {
     e.preventDefault();
-
+    //close burger by tap menu links on mobile
     document.documentElement.classList.contains('menu-open') ? document.documentElement.classList.remove('menu-open') :
       null;
 
